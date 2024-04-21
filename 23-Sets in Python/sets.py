869e1836b76634f1eaefc3374670c447ff916a18
Shivansh Bajpai <shivanshbajpai31@gmail.com>
2024-04-21 12:34:48 +0530

    commit

---
+++ 23-Sets in Python/sets.py
@@ -0,0 +1,14 @@
+s={2 ,4, 4,6}
+print(s)
+
+info={"Carla", 19, False, 5.9, 1.9}
+print(info)
+
+#Quick Quizz
+#Try to create an empty set.Check using type function whether the type of variable is set
+Shivansh=set()
+print(type(Shivansh))
+
+#Accessing set items
+for value in info:
+    print(info)
