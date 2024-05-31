f8d93165fa2724fdebd253e7b818689e96457f53
Shivansh Bajpai <shivanshbajpai31@gmail.com>
2024-05-31 18:53:13 +0530

    commit

---
+++ 25-Dictionaries in Python/dictionary.py
@@ -0,0 +1,14 @@
+dic={
+    344:"Shivansh",
+    411:"Afreen",
+    78:"Neha"
+}
+print (dic[344])
+
+#Accesing multiple values:
+info = {'name':'Karan', 'age':19, 'eligible':True}
+print(info.values())
+print(info.keys())
+
+for key in info.keys():
+    print(info[key])
