2c210624c7bce724726b25f9423e83544014b7ca
Shivansh Bajpai <shivanshbajpai31@gmail.com>
2024-06-05 22:49:59 +0530

    commit

--- 30-Raising custom errors/raise.py
+++ 30-Raising custom errors/raise.py
@@ -2,3 +2,11 @@ a=int(input("Enter any valyes between 5 and 9"))
 
 if(a<5 or a>9):
     raise ValueError("Value should be between 5 and 9")
+
+
+while True:
+    b=input(int("Enter any value between 5 and 9 :"))
+    if a in ('5','6','7','8','9','QUit','quit'):
+        break
+    else:
+        raise ValueError("Value should be between 5 and 9")
