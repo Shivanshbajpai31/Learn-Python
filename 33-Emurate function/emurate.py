545aeea402b41ac33c029f8dae9da3d3381b9b3c
Shivansh Bajpai <shivanshbajpai31@gmail.com>
2024-06-08 20:41:55 +0530

    committed

---
+++ 33-Emurate function/emurate.py
@@ -0,0 +1,13 @@
+marks=[1,3,5,67,69,98,3,45]
+
+#index=0
+#for mark in marks:
+    #print(mark)
+    #if (index ==5):
+      #  print ("Shivansh well Done")
+   # index +=1
+
+for index, mark in enumerate(marks, start=1):
+    print(mark)
+    if(index==5):
+        print("Shivansh well done!!!")
