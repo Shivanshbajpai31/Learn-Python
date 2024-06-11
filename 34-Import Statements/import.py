7e03eaba58a916ce4c08679351f7a1c84b5c267b
Shivansh Bajpai <shivanshbajpai31@gmail.com>
2024-06-11 18:58:05 +0530

    committed

---
+++ 34-Import Statements/import.py
@@ -0,0 +1,26 @@
+#import function
+#import math
+#result=math.sqrt(9)
+#print(result)
+
+#from keyword
+from math import sqrt
+result=sqrt(9)
+print(result)
+
+#importing everything
+from math import *
+result2=sqrt(9)
+print(result2)
+print(pi)
+
+#as keyword
+import math as m
+result3= m.sqrt(9)
+result3=m.sqrt(9)
+print(result3)
+print(m.pi)
+
+#dir function
+import math
+print(dir(math))
