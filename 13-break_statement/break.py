f7663087e3254788a3b0c056ab0c711fdcf3fdd0
Shivansh Bajpai <shivanshbajpai31@gmail.com>
2024-06-11 19:12:46 +0530

    committed
    committed

---
+++ 13-break_statement/break.py
@@ -0,0 +1,15 @@
+# for i in range(1,101,1):
+#     print(i, end="")
+#     if(i==50):
+#         break
+#     else:
+#         print("Missingippi")
+
+# print("Thank you")
+num = 0
+for i in range(10): 
+	num += 1
+	if num == 8: 
+		break
+	print("The num has value:", num) 
+print("Out of loop") 
---
+++ 13-break_statement/continue.py
@@ -0,0 +1,4 @@
+for i in [2,4,3,6,8]:
+    if(i%2!=0):
+        continue 
+    print(i)
